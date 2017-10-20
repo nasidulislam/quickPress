cb01f1c56a00ca071a685ec69c5e3aae039db66f
Nasidul <nasidul_islam@anfcorp.com>
2017-10-20 12:27:04 -0400

    game-scoring: initial setup for game.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
-var lightSwitch, settings;
+var lightSwitch, settings, gameButton;
 
 settings = {
+    gameButtonClass: '.game-circle',
     lightsSliderClass: '.game-lights__switch-slider'
 }
 
 // buttons
 lightSwitch = document.querySelector(settings.lightsSliderClass);
+gameButton = document.querySelector(settings.gameButtonClass);
 
 /* Begin Function Declarations */
 
+function theGame(event) {
+    console.log('game');
+}
+
 function toggleLight(event) {
-    console.log(event);
+    console.log('toggle light');
 }
 
 /* End Function Declarations */
@@ -18,5 +24,6 @@ function toggleLight(event) {
 /* Begin Event Listeners */
 
 lightSwitch.addEventListener('click', toggleLight);
+gameButton.addEventListener('click', theGame);
 
 /* End Event Listeners */
