417b719adb08cb5a87dae3c4251dfb496b1c608a
Nasidul <nasidul_islam@anfcorp.com>
2017-10-20 15:51:42 -0400

    game-scoring: do basic setup of game.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,45 @@
-var lightSwitch, settings, gameButton;
+var $lightSwitch, settings, $gameButton, $gameControls;
 
 settings = {
     gameButtonClass: '.game-circle',
-    lightsSliderClass: '.game-lights__switch-slider'
+    gameControlsContainerClass: '.game-controls__container',
+    lightsSliderClass: '.game-lights__switch-slider',
+    circlePoint: '.point',
+    builtTimingArray: []
 }
 
 // buttons
-lightSwitch = document.querySelector(settings.lightsSliderClass);
-gameButton = document.querySelector(settings.gameButtonClass);
+$lightSwitch = document.querySelector(settings.lightsSliderClass);
+$gameButton = document.querySelector(settings.gameButtonClass);
+
+// Elements
+$gameControls = document.querySelector(settings.gameControlsContainerClass);
+$point = document.querySelector(settings.circlePoint);
 
 /* Begin Function Declarations */
 
 function theGame(event) {
-    console.log('game');
+    $gameControls.classList.add('game-start');
+    handleLoopDecrement();
+}
+
+function handleLoopDecrement() {
+    $point.style.cssText = 'animation-duration: ' + settings.builtTimingArray.pop() + 's';
+}
+
+function buildTimingArray() {
+    var animationTiming, initial, i, builtArray;
+
+    animationTiming = [5, 4, 3, 2];
+    animationTiming = [5, 4.5, 4, 3.5, 3, 2.5, 2];
+    builtArray = [];
+
+    for(i = 0; i < 50; i++) {
+        initial = animationTiming[animationTiming.length - 1];
+        animationTiming.push(initial/1.2);
+    }
+
+    settings.builtTimingArray = animationTiming.reverse();
 }
 
 function toggleLight(event) {
@@ -23,7 +50,8 @@ function toggleLight(event) {
 
 /* Begin Event Listeners */
 
-lightSwitch.addEventListener('click', toggleLight);
-gameButton.addEventListener('click', theGame);
+document.addEventListener('DOMContentLoaded', buildTimingArray);
+$lightSwitch.addEventListener('click', toggleLight);
+$gameButton.addEventListener('click', theGame);
 
 /* End Event Listeners */
