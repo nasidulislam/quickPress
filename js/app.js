74ea5f8abddf93fec30e99a063a63f92fa6f411b
Nasidul <nasidul_islam@anfcorp.com>
2017-10-27 15:12:05 -0400

    toggle-lights: setup js for lights toggle. move classes added/removed to settings object. reformatted code. minor cleanup.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 var $lightSwitch, settings, $gameButton, $gameControls;
 
 settings = {
-    gameButtonClass: '.game-circle',
-    gameControlsContainerClass: '.game-controls__container',
-    lightsSliderClass: '.game-buttons__lights-slider',
-    circlePoint: '.point',
-    scoreCardClass: '.game-circle__scorecard',
-    builtTimingArray: [],
-    resetButtonClass: '.game-buttons__reset'
+	gameButtonClass: '.game-circle',
+	gameControlsContainerClass: '.game-controls__container',
+	lightsSliderClass: '.game-buttons__lights-slider',
+	circlePoint: '.point',
+	scoreCardClass: '.game-circle__scorecard',
+	builtTimingArray: [],
+	resetButtonClass: '.game-buttons__reset',
+	primaryContentClass: '.primary-content',
+	gameStart: 'game-start',
+	lightsToggle: 'toggle-lights'
 };
 
 // buttons
@@ -19,65 +22,66 @@ $resetButton = document.querySelector(settings.resetButtonClass);
 $gameControls = document.querySelector(settings.gameControlsContainerClass);
 $point = document.querySelector(settings.circlePoint);
 $scoreCard = document.querySelector(settings.scoreCardClass);
+$primaryContent = document.querySelector(settings.primaryContentClass);
 
 /* Begin Function Declarations */
 
 function theGame() {
-    $gameControls.classList.add('game-start');
-    handleLoopDecrement();
-    handleScoring();
+	$gameControls.classList.add(settings.gameStart);
+	handleLoopDecrement();
+	handleScoring();
 }
 
 function handleLoopDecrement() {
-    $point.style.cssText = 'animation-duration: ' + settings.builtTimingArray.pop() + 's';
+	$point.style.cssText = 'animation-duration: ' + settings.builtTimingArray.pop() + 's';
 }
 
 function handleScoring() {
-    var currentScore, newScore;
+	var currentScore, newScore;
 
-    currentScore = getCurrentScore();
-    newScore = currentScore + 1;
-    setScore(newScore);
+	currentScore = getCurrentScore();
+	newScore = currentScore + 1;
+	setScore(newScore);
 }
 
 function handleReset() {
-    setScore(0);
-    $gameControls.classList.remove('game-start');
-    console.log('reset');
+	setScore(0);
+	buildTimingArray();
+	$gameControls.classList.remove(settings.gameStart);
 }
 
 function buildTimingArray() {
-    var animationTiming, initial, i;
+	var animationTiming, initial, i;
 
-    animationTiming = [5, 4.5, 4, 3.5, 3, 2.5, 2];
+	animationTiming = [5, 4.5, 4, 3.5, 3, 2.5, 2];
 
-    for(i = 0; i < 50; i++) {
-        initial = animationTiming[animationTiming.length - 1];
-        animationTiming.push(initial/1.2);
-    }
+	for (i = 0; i < 50; i++) {
+		initial = animationTiming[animationTiming.length - 1];
+		animationTiming.push(initial / 1.2);
+	}
 
-    settings.builtTimingArray = animationTiming.reverse();
+	settings.builtTimingArray = animationTiming.reverse();
 }
 
 function handleReload() {
-    buildTimingArray();
-    handleReset();
+	buildTimingArray();
+	handleReset();
 }
 
 function toggleLight() {
-    console.log('toggle light');
+	$primaryContent.classList.toggle(settings.lightsToggle);
 }
 
 function getCurrentScore() {
-    var currentScore;
+	var currentScore;
 
-    currentScore = parseInt($scoreCard.innerHTML);
-    settings.currentScore = currentScore;
-    return currentScore;
+	currentScore = parseInt($scoreCard.innerHTML);
+	settings.currentScore = currentScore;
+	return currentScore;
 }
 
 function setScore(score) {
-    $scoreCard.innerHTML = score;
+	$scoreCard.innerHTML = score;
 }
 
 /* End Function Declarations */
