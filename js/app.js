c717767d0c1197e9a27682df05e31fed35d13bc8
Nasidul <nasidul_islam@anfcorp.com>
2017-11-01 14:42:52 -0400

    game-points: cleanup js.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,9 @@ var $lightSwitch, settings, $gameButton, $gameControls;
 
 settings = {
 	// selectors
-	gameButtonClass: '.game-circle',
 	gameControlsContainerClass: '.game-controls__container',
 	lightsSliderClass: '.game-buttons__lights-slider',
 	scoreCardClass: '.game-circle__scorecard',
-	circlePoint: '.point',
-	circlePunt: '.punt',
 
 	// buttons
 	resetButtonClass: '.game-buttons__reset',
@@ -17,7 +14,6 @@ settings = {
 	// classes
 	gameStart: 'game-start',
 	lightsToggle: 'toggle-lights',
-	animatePunt: 'animate-punt',
 
 	// others
 	builtTimingArray: []
@@ -25,13 +21,10 @@ settings = {
 
 // buttons
 $lightSwitch = document.querySelector(settings.lightsSliderClass);
-$gameButton = document.querySelector(settings.gameButtonClass);
 $resetButton = document.querySelector(settings.resetButtonClass);
 
 // Elements
 $gameControls = document.querySelector(settings.gameControlsContainerClass);
-$point = document.querySelector(settings.circlePoint);
-$punt = document.querySelector(settings.circlePunt);
 $scoreCard = document.querySelector(settings.scoreCardClass);
 $primaryContent = document.querySelector(settings.primaryContentClass);
 $body = document.querySelector(settings.bodyClass);
@@ -42,11 +35,6 @@ function theGame () {
 	$gameControls.classList.add(settings.gameStart);
 	handleLoopDecrement();
 	handleScoring();
-	handlePuntPlacement();
-}
-
-function handleLoopDecrement () {
-	$point.style.cssText = 'animation-duration: ' + settings.builtTimingArray.pop() + 's';
 }
 
 function handleScoring () {
@@ -59,25 +47,10 @@ function handleScoring () {
 
 function handleReset () {
 	setScore(0);
-	buildTimingArray();
 	$gameControls.classList.remove(settings.gameStart);
 }
 
-function buildTimingArray () {
-	var animationTiming, initial, i;
-
-	animationTiming = [5, 4.5, 4, 3.5, 3, 2.5, 2];
-
-	for (i = 0; i < 50; i++) {
-		initial = animationTiming[animationTiming.length - 1];
-		animationTiming.push(initial / 1.2);
-	}
-
-	settings.builtTimingArray = animationTiming.reverse();
-}
-
 function handleReload() {
-	buildTimingArray();
 	handleReset();
 }
 
@@ -97,28 +70,12 @@ function setScore (score) {
 	$scoreCard.innerHTML = score;
 }
 
-function handlePuntPlacement () {
-	$punt.style.cssText = 'animation-play-state: running';
-	setTimeout(function() {
-		$punt.style.cssText = 'animation-play-state: paused';
-	}, 25);
-	getCurrentAngle();
-}
-
-function getCurrentAngle () {
-	var style, transformMatrix;
-
-	style = window.getComputedStyle($point, null);
-	transformMatrix = style.getPropertyValue('transform');
-	console.log(transformMatrix);
-}
 /* End Function Declarations */
 
 /* Begin Event Listeners */
 
 document.addEventListener('DOMContentLoaded', handleReload);
 $lightSwitch.addEventListener('click', toggleLight);
-$gameButton.addEventListener('click', theGame);
 $resetButton.addEventListener('click', handleReset);
 
 /* End Event Listeners */
