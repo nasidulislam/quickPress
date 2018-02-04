b2a4346781ca5194a1a7fcae5f3ca650b4d17bb6
Nasidul Islam <nasidulislam@Nasiduls-MacBook-Pro.local>
2018-02-04 11:52:00 -0500

    implement-require: move remaining tries functionality to its own module.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,21 @@
 define(function (require) {
     var settings,
-        $resetButton, $lightSwitch, $gameControls, $svgCircle, $scoreCard, $svgElement, $remainingTriesScore,
-        $point, $finalScore, $remainingTriesElement;
+        $resetButton, $lightSwitch, $gameControls, $svgCircle, $remainingTriesScore;
 
     settings = {
         // selectors
         gameControlsContainerClass: '.game-controls__container',
-        remainingTriesContainerClass: '.game-score__remaining-tries',
         lightsSliderClass: '.game-buttons__lights-slider',
-        scoreCardClass: '.game-circle__scorecard',
         svgCircleClass: '.game-circle',
-        circlePoint: '.point',
-        circlePunt: '.punt',
-        finalScoreClass: '.game-score__final',
         remainingTriesScoreClass: '.game-score__life',
 
         // buttons
         resetButtonClass: '.game-buttons__reset',
-        primaryContentClass: '.primary-content',
 
         // classes
         gameStart: 'game-start',
         lightsToggle: 'toggle-lights',
-        vibrateClass: 'vibrate-class',
-        redColorClass: 'color-class-red'
+        vibrateClass: 'vibrate-class'
     };
 
     // buttons
@@ -32,19 +24,15 @@ define(function (require) {
 
     // Elements
     $gameControls = document.querySelector(settings.gameControlsContainerClass);
-    $scoreCard = document.querySelector(settings.scoreCardClass);
     $svgCircle = document.querySelector(settings.svgCircleClass);
-    $point = document.querySelector(settings.circlePoint);
-    $svgElement = document.getElementsByTagName('svg')[0];
     $remainingTriesScore = document.querySelector(settings.remainingTriesScoreClass);
-    $finalScore = document.querySelector(settings.finalScoreClass);
-    $remainingTriesElement = document.querySelector(settings.remainingTriesContainerClass);
 
     // modules
     var placement = require('modules/placement');
     var frequency = require('modules/frequency');
     var score = require('modules/score');
     var util = require('modules/util');
+    var remainingTries = require('modules/remainingTries');
 
     /* Begin Function Declarations */
 
@@ -53,7 +41,7 @@ define(function (require) {
 
         if (score.isValidScore()) {
             score.handleScoring();
-            frequency.handlePointFrequency($point);
+            frequency.handlePointFrequency();
             placement.handlePuntPlacement();
         } else {
             handleInvalidScore();
@@ -61,44 +49,12 @@ define(function (require) {
     }
 
     function handleInvalidScore() {
-        handleRemainingTries();
+        remainingTries.handleRemainingTries();
     }
 
     function handleReload() {
         util.handleReset();
         util.getCurrentLocation();
-    }
-
-    function handleRemainingTries() {
-        var newRemainingTries, customEvent;
-
-        // decrease score
-        newRemainingTries = score.getCurrentScore($remainingTriesScore) - 1;
-        score.setScore($remainingTriesScore, newRemainingTries);
-
-        // dispatch custom event that handles animation on wrong hits
-        customEvent = new Event('quickPress: wrong-hit');
-        $remainingTriesScore.dispatchEvent(customEvent);
-
-        // make remaining tries section red when user has 0 tries left
-        if (score.getCurrentScore($remainingTriesScore) === 0) {
-            $remainingTriesElement.classList.add(settings.redColorClass);
-        }
-
-        // handles end game functionality --> should probably make its own function
-        if (score.getCurrentScore($remainingTriesScore) < 0) {
-            alert('game over');
-            score.setScore($remainingTriesScore, 0);
-            $finalScore.innerHTML = 'Your Final Score: <span class="final-score">' + score.getCurrentScore($scoreCard) + '</span>';
-            $svgElement.pauseAnimations();
-        }
-    }
-
-    function handleRemainingTriesAnimation() {
-        $remainingTriesElement.classList.add(settings.vibrateClass);
-        $remainingTriesElement.addEventListener('animationend', function () {
-            $remainingTriesElement.classList.remove(settings.vibrateClass);
-        });
     }
 
     /* End Function Declarations */
@@ -109,7 +65,7 @@ define(function (require) {
     $lightSwitch.addEventListener('click', util.toggleLight);
     $resetButton.addEventListener('click', util.handleReset);
     $svgCircle.addEventListener('click', theGame);
-    $remainingTriesScore.addEventListener('quickPress: wrong-hit', handleRemainingTriesAnimation);
+    $remainingTriesScore.addEventListener('quickPress: wrong-hit', remainingTries.handleRemainingTriesAnimation);
 
     /* End Event Listeners */
 
--- js/modules/frequency.js
+++ js/modules/frequency.js
@@ -1,34 +1,39 @@
 define(function () {
-    var publicMembers = {
-        handlePointFrequency: function (el) {
-            var currentDuration;
+    var settings = {
+            circlePoint: '.point'
+        },
 
-            currentDuration = publicMembers.getRotationFrequency(el);
+        publicMembers = {
+            handlePointFrequency: function () {
+                var currentDuration, $point;
 
-            // bypass initial duration value being set to nothing on svg element
-            if (isNaN(currentDuration)) {
-                currentDuration = 20;
+                $point = document.querySelector(settings.circlePoint);
+                currentDuration = publicMembers.getRotationFrequency($point);
+
+                // bypass initial duration value being set to nothing on svg element
+                if (isNaN(currentDuration)) {
+                    currentDuration = 20;
+                }
+
+                publicMembers.setRotationFrequency($point, currentDuration / 2); //TODO : write a difficulty implementation function
+            },
+            getRotationFrequency: function (el) {
+                var attrs, freq, freqNumber;
+
+                attrs = el.attributes;
+                freq = attrs.dur.value;
+                freqNumber = parseFloat(freq.substr(0, freq.length - 1));
+
+                return freqNumber;
+            },
+
+            setRotationFrequency: function (el, freq) {
+                var attrs;
+
+                attrs = el.attributes;
+                attrs.dur.value = freq + 's';
             }
-
-            publicMembers.setRotationFrequency(el, currentDuration / 2); //TODO : write a difficulty implementation function
-        },
-        getRotationFrequency: function (el) {
-            var attrs, freq, freqNumber;
-
-            attrs = el.attributes;
-            freq = attrs.dur.value;
-            freqNumber = parseFloat(freq.substr(0, freq.length - 1));
-
-            return freqNumber;
-        },
-
-        setRotationFrequency: function (el, freq) {
-            var attrs;
-
-            attrs = el.attributes;
-            attrs.dur.value = freq + 's';
-        }
-    };
+        };
 
     return publicMembers;
 });
