793730b73529b1e7d2097d21c2c4c265954dac42
Nasidul <nasidul_islam@anfcorp.com>
2018-02-06 15:50:58 -0500

    difficulty-function: implement simple difficulty algorithm and fix code formatting issue.

--- js/modules/frequency.js
+++ js/modules/frequency.js
@@ -1,39 +1,53 @@
 define(function () {
-    var settings = {
-            circlePoint: '.point'
-        },
+	var settings = {
+			circlePoint: '.point'
+		},
 
-        publicMembers = {
-            handlePointFrequency: function () {
-                var currentDuration, $point;
+		publicMembers = {
+			handlePointFrequency: function () {
+				var currentFreq, $point, newFreq;
 
-                $point = document.querySelector(settings.circlePoint);
-                currentDuration = publicMembers.getRotationFrequency($point);
+				$point = document.querySelector(settings.circlePoint);
+				currentFreq = publicMembers.getRotationFrequency($point);
 
-                // bypass initial duration value being set to nothing on svg element
-                if (isNaN(currentDuration)) {
-                    currentDuration = 20;
-                }
+				// bypass initial duration value being set to nothing on svg element
+				if (isNaN(currentFreq)) {
+					currentFreq = 20;
+				}
 
-                publicMembers.setRotationFrequency($point, currentDuration / 2); //TODO : write a difficulty implementation function
-            },
-            getRotationFrequency: function (el) {
-                var attrs, freq, freqNumber;
+				newFreq = publicMembers.generateNewFrequency(currentFreq);
+				publicMembers.setRotationFrequency($point, newFreq);
+			},
 
-                attrs = el.attributes;
-                freq = attrs.dur.value;
-                freqNumber = parseFloat(freq.substr(0, freq.length - 1));
+			getRotationFrequency: function (el) {
+				var attrs, freq, freqNumber;
 
-                return freqNumber;
-            },
+				attrs = el.attributes;
+				freq = attrs.dur.value;
+				freqNumber = parseFloat(freq.substr(0, freq.length - 1));
 
-            setRotationFrequency: function (el, freq) {
-                var attrs;
+				return freqNumber;
+			},
 
-                attrs = el.attributes;
-                attrs.dur.value = freq + 's';
-            }
-        };
+			setRotationFrequency: function (el, freq) {
+				var attrs;
 
-    return publicMembers;
+				attrs = el.attributes;
+				attrs.dur.value = freq + 's';
+			},
+
+			generateNewFrequency: function (freq) {
+				if (freq >= 10) {
+					return (freq / 2);
+				} else if (freq >= 2) {
+					return (freq / (4 / 3));
+				} else if (freq >= 0.6) {
+					return (freq / 1.125);
+				} else {
+					return (freq / 1.125);
+				}
+			}
+		};
+
+	return publicMembers;
 });
